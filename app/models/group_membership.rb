accc8b14f47fb3fb513c97e64dbd6a6f83adf8c7
JulissaJ <julissa.jansen@gmail.com>
2014-03-25 13:42:45 -0400

    passed tests for group_membership table

--- app/models/group_membership.rb
+++ app/models/group_membership.rb
@@ -1,4 +1,7 @@
 class GroupMembership < ActiveRecord::Base
+	validates :user, presence: true
+	validates :interest_group, presence: true
 	belongs_to :interest_group
+	belongs_to :user
 	
 end
--- spec/models/group_membership_spec.rb
+++ spec/models/group_membership_spec.rb
@@ -1,31 +1,27 @@
 require 'spec_helper'
 
+describe GroupMembership do
+  let(:user){User.create(first_name: 'Bob', last_name: 'Bill', email: 'test@test.com')}
+  let(:interest_group){InterestGroup.create(name: 'Best group ever', creator: user)}
+  let(:group_membership){GroupMembership.new(user: user, interest_group: interest_group)}
 
+  it 'should belong to a user' do
+    expect(group_membership).to respond_to(:user)
+  end
 
+  it 'should belong to a interest group' do
+    expect(group_membership).to respond_to(:interest_group)
+  end
 
-
-
-
-
-# describe GroupMembership do
-#    describe 'validation' do
-#   	context "when given valid attributes" do
-#   		it 'is valid' do
-#   		user = User.new(
-# 			first_name: 'Steve',
-# 			last_name: "Smith",
-# 			roles: "launcher",
-# 			fun_fact: "I'm a unicorn",
-# 			email: "imsecretlyaunicorn@gmail.com"
-# 			) 
-# 			group = InterestGroup.new(
-#         name: "Excuses",
-#         user: user
-#       )
-#       groupmembership = GroupMembership.new(user: user, group: group)
-#   			expect(groupmembership.user).to eql user
-#   			expect(groupmembership.group).to eql group
-#   		end
-#   	end
-#   end
-# end
+  it 'should validate the presence of a user' do
+    group_membership.user = nil
+    expect(group_membership).to_not be_valid
+    expect(group_membership.errors[:user]).to include("can't be blank")
+  end
+  
+  it 'should validate the presence of an interest group' do
+    group_membership.interest_group = nil
+    expect(group_membership).to_not be_valid
+    expect(group_membership.errors[:interest_group]).to include("can't be blank")
+  end
+end
